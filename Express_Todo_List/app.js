401dbff7282678482a09934970af8c80af6fe2ab
Iniabasi <43305018+Iniabasi@users.noreply.github.com>
2021-08-26 09:26:40 -0400

    Generating dynamic content with ejs
    Routing Html Pages
    EJS template engine

---
+++ Express_Todo_List/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.listen(3000);
+
+app.get('/', (req,res)=>{
+    const items =[
+        {name: 'mobile phone', price: 1000},
+        {name: 'book', price: 140},
+        {name: 'ipad', price: 1020},
+
+    ]
+ 
+    res.render('index', {items});
+
+
+    
+
+})
+app.get('/add-item', (req,res)=>{
+    res.render('add-item');
+})
+
+app.use((req, res)=>{
+    res.render('error');
+})
