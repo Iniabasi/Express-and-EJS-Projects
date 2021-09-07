e218f307172de2d2816a75d90298e6cdc0bc0332
Iniabasi <43305018+Iniabasi@users.noreply.github.com>
2021-09-06 23:52:19 -0400

    Setting up MongoDB
    Setting Up MongoDB, creating clusters, adding users and linking it with the application

--- Express_Todo_List/app.js
+++ Express_Todo_List/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const mongodb= 'mongodb+srv://Iniabasi:Ini123@cluster0.ejnpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 app.set('view engine', 'ejs');
 app.listen(3000);
 
